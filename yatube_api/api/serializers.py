d4d4609fb3dbbb9ce325b809665a94d4266bb162
lacoast <pulse.emotion@yandex.ru>
2021-07-12 01:09:52 +0300

    3

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Comment, Post
+from .models import Comment, Post, Follow, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,3 +17,24 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field = "username",
+        read_only = True,)
+    following = serializers.SlugRelatedField(
+        slug_field = "username",
+        read_only = True,)
+        
+    class Meta:
+        fields = ("user", "following")
+        model = Follow
+        
+        
+class GroupSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        fields = ("title", "slug", "description")
+        model = Group
+        
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-#from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
-#from .permissions import IsOwnerOrReadOnly
-from .models import Post
-from .serializers import CommentSerializer, PostSerializer
+from .models import Post, Comment, Follow, Group
+from .serializers import CommentSerializer, PostSerializer, FollowSerializer, GroupSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    #permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -18,7 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    #permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs['id'])
@@ -29,3 +28,22 @@ class CommentViewSet(viewsets.ModelViewSet):
         post = get_object_or_404(Post, id=self.kwargs['id'])
         serializer.save(author=self.request.user, post=post)
 
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    #filter_backends = [filters.SearchFilter], Не понимаю в чём тут проблема. Падает ошибка, если раскоменнтить
+    search_fields = ["user_username"]
+    
+    def get_queryset():
+        return self.request.objects.filter()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+        
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticated]
+    
